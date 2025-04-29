433523849cd54d731b813f4708363763de1295e4
Abdullah <177544140+abdullah-per@users.noreply.github.com>
2025-04-29 12:14:26 +0500

    tweak

--- scripts/AIChat.js
+++ scripts/AIChat.js
@@ -2,33 +2,6 @@ const chatOutput = document.querySelector(".chat--output");
 const chatInput = document.querySelector("#chat");
 const chatForm = document.querySelector(".chat--form");
 
-const baseSystemPrompt = `
-Your messages must be short.
-Use multi-line format.
-Keep sentences under two lines, keeping in mind that the display in which the output will be shown is 60 characters long.
-Use lists whenever possible.
-You are a knowledgeable AI Chat Assitant for PAE and are here to answer any questions..
-Respond in a clear, friendly, professional tone.
-If you do not know, say so politely.
-Refer users to the contact form.
-`;
-
-const businessInfoPrompt = `
-PAE provides predictive analytics for:
-- Predictive analytics
-- Operational optimization
-- Business intelligence
-
-Our mission:
-- Empower businesses to make data-driven
-  decisions
-
-Core offerings:
-1. Predictive Analytics Engine
-2. Sales Automation Platform
-3. Smart ERP Modules
-`;
-
 
 async function chatWithBackend(prompt) {
     const backendURL = "https://pae-chat-975843523063.us-central1.run.app/";
